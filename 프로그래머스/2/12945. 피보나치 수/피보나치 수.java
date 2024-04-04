7a0a4e4d2931ab9448687fbf6c0c9afc58573669
staer77 <103422426+staer77@users.noreply.github.com>
2024-04-04 13:22:54 +0900

    [level 2] Title: 피보나치 수, Time: 1.78 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int f1 = 0;
+        int f2 = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            answer = (f1 + f2) % 1234567;
+            f1 = f2;
+            f2 = answer;
+        }
+        return answer;
+    }
+}
